d98b9e39f27e1b807ab52c83f828d22fdc63d62f
stefan <stefansalla75@gmail.com>
2024-06-24 22:45:16 -0300

    atualisaçao de pacote

--- lib/ficha_de_ponto_pk.dart
+++ lib/ficha_de_ponto_pk.dart
@@ -1,17 +1,21 @@
 library ficha_de_ponto_pk;
 
 class Ponto {
-  final int id;
+  final String id;
   final String nome;
   final String imagePath;
   final bool isSaida;
   final DateTime dataHora;
 
-  Ponto({
-    required this.id,
-    required this.nome,
-    required this.imagePath,
-    required this.isSaida,
-    required this.dataHora,
-  });
+  Ponto(
+    this.id,
+     this.nome,
+     this.imagePath,
+     this.isSaida,
+     this.dataHora,
+  );
+
+  static Ponto fromMap(Map<String, dynamic> map){
+    return Ponto( map['id'], map['nome'], map['imagePath'], map['isSaida'], map['dataHora']);
+  }
 }
